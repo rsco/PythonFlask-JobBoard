1d8847672b9b37284c3706ab436b4cee435a0180
Rodrigo Oliveira <rodrigo.oliveira@landisgyr.com>
2019-12-10 15:58:46 -0300

    Completed Module Identification:876259
    Change-Id: I9b2817238a1b2a2c85cb4e0a302bbb3cd4b1428d

--- jobs/app.py
+++ jobs/app.py
@@ -18,9 +18,9 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
        results = connection.commit()
    else:
-        results = cursor.fetchone() if single else cursor.fetchball()
+        results = cursor.fetchone() if single else cursor.fetchall()
 
-    cursor.close();
+    cursor.close()
     return results
 
 @app.teardown_appcontext
